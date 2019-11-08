4e35e5bd3544f7fddffa9102ba159579627befa0
Carlos-fernandez <fernandez.carlos40@gmail.com>
2019-11-08 02:12:53 -0500

    clean up classes
    made the classes a bit more flexible. I'll add more flexiblity to it

--- Simulations/Rainfall Simulations.py
+++ Simulations/Rainfall Simulations.py
@@ -23,84 +23,77 @@ class ColorPalette:
     """
     Collection of objects/functions that support color changing of LED matrix
     """
-    active_color = (255, 255, 255)
-    colors = [
-        ['white', (255, 255, 255)],
-        ['red', (255, 0, 00)],
-        ['orange', (255, 127, 0)],
-        ['yellow', (255, 255, 0)],
-        ['green', (0, 255, 0)],
-        ['blue', (0, 0, 255)],
-        ['indigo', (75, 0, 130)],
-        ['violet', (148, 0, 211)]
-    ]
-    color_palette = {}
+    def __init__(self):
+        self.active_color = (255, 255, 255)
+        self.colors = [
+            ['white', (255, 255, 255)],
+            ['red', (255, 0, 00)],
+            ['orange', (255, 127, 0)],
+            ['yellow', (255, 255, 0)],
+            ['green', (0, 255, 0)],
+            ['blue', (0, 0, 255)],
+            ['indigo', (75, 0, 130)],
+            ['violet', (148, 0, 211)]
+        ]
+        self.color_palette = {}
 
-    def create_palette():
-        for color, xpos in zip(ColorPalette.colors, range(20, 230+1, 30)):
-            ColorPalette.color_palette[color[0]] = [color[1], pygame.draw.circle(background, color[1], (xpos, 460), 10)]
+    def create_palette(self):
+        for color, xpos in zip(self.colors, range(20, 230+1, 30)):
+            self.color_palette[color[0]] = [color[1], pygame.draw.circle(background, color[1], (xpos, 460), 10)]
 
     def set_active_color():
-        for color in ColorPalette.color_palette.keys():
-            if ColorPalette.color_palette[color][1].collidepoint(pygame.mouse.get_pos()) and event.type == MOUSEBUTTONDOWN:
-                ColorPalette.active_color = ColorPalette.color_palette[color][0]
-                test_object = pygame.draw.circle(background, ColorPalette.active_color, (20, 20), 10)
+        for color in self.color_palette.keys():
+            if self.color_palette[color][1].collidepoint(pygame.mouse.get_pos()) and event.type == MOUSEBUTTONDOWN:
+                self.active_color = self.color_palette[color][0]
+                test_object = pygame.draw.circle(background, self.active_color, (20, 20), 10)
 
     def set_color_drop():
-        ColorPalette.active_color = ColorPalette.color_palette[color][2]
-        test_object = pygame.draw.circle(background, ColorPalette.active_color, (20, 20), 10)
+        self.active_color = self.color_palette[color][2]
+        test_object = pygame.draw.circle(background, self.active_color, (20, 20), 10)
 
 
 class LED:
     """
     Collection of functions create a grid of circles that represent an LED matrix and initiate color-change behavior
     """
-    led_grid = {}
-
-    def create_led_grid():
+    def __init__(self):
+        self.led_grid = {}
+        self.led_num = 8
+    def create_led_grid(self):
         x_origin, y_origin = 145, 65
         for letter, coord in zip(ascii_lowercase[0:8], range(LED_NUM)):
             x = x_origin + (coord * 50)
-            LED.led_grid[letter + '1'] = [pygame.draw.circle(background, (ColorPalette.active_color), (x, y_origin), 20), False, x, y_origin]
+            self.led_grid[letter + '1'] = [pygame.draw.circle(background, (ColorPalette().active_color), (x, y_origin), 20), False, x, y_origin]
             for coord in range(1, LED_NUM):
                 y = y_origin + (coord * 50)
-                LED.led_grid[letter + str(coord+1)] = [pygame.draw.circle(background, (ColorPalette.active_color), (x, y), 20), False, x, y]
+                self.led_grid[letter + str(coord+1)] = [pygame.draw.circle(background, (ColorPalette().active_color), (x, y), 20), False, x, y]
 
-    def toggle_led():
-        for led in LED.led_grid.values():
+    def toggle_led(self):
+        for led in self.led_grid.values():
             if led[0].collidepoint(pygame.mouse.get_pos()) and event.type == MOUSEBUTTONDOWN:
                 led[1] = True
         for led in LED.led_grid.keys():
-            if LED.led_grid[led][1] == True:
-                LED.led_grid[led][0] = pygame.draw.circle(background, (ColorPalette.active_color), (LED.led_grid[led][2], LED.led_grid[led][3]), 20)
-                LED.led_grid[led][1] = False
+            if self.led_grid[led][1] == True:
+                self.led_grid[led][0] = pygame.draw.circle(background, (ColorPalette.active_color), (self.led_grid[led][2], self.led_grid[led][3]), 20)
+                self.led_grid[led][1] = False
 
-    def rain_drop():
+    def rain_drop(self):
         x_origin, y_origin = 145, 65
         for letter, coord in zip(ascii_lowercase[0:8], range(LED_NUM)):
             y = y_origin + (coord * 50)
-            LED.led_grid[letter + '1'] = [pygame.draw.circle(background, (0, 0, 255), (x_origin, y), 20), False, x_origin, y]
-            #LED.led_grid[letter + '2'] = [pygame.draw.circle(background, (255, 255, 255), (x, y_origin), 20), False, x, y_origin]
+            self.led_grid[letter + '1'] = [pygame.draw.circle(background, (0, 0, 255), (x_origin, y), 20), False, x_origin, y]
             pygame.display.update()
             clock.tick(15)
             screen.blit(background, (0, 0))
-            LED.led_grid[letter + '1'] = [pygame.draw.circle(background, (255, 255, 255), (x_origin, y), 20), False, x_origin, y]
+            self.led_grid[letter + '1'] = [pygame.draw.circle(background, (255, 255, 255), (x_origin, y), 20), False, x_origin, y]
             pygame.display.update()
             clock.tick(15)
-            #clock.tick(15)
-            #screen.blit(background, (0, 0))
-#initiate color palette and LED matrix
 
 if __name__ == '__main__':
-    ColorPalette.create_palette()
-    LED.create_led_grid()
+    ColorPalette().create_palette()
+    LED().create_led_grid()
     #core loop
     while True:
-        #clock.tick(1000)
-        #screen.blit(background, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
-        #ColorPalette.set_active_color()
-        #LED.toggle_led
-        LED.rain_drop()
-        #pygame.display.update()
+        LED().rain_drop()
