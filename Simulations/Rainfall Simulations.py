e6cdc75552f3dd896f97c0579fe9b5112a3f3ba6
Carlos-fernandez <fernandez.carlos40@gmail.com>
2019-11-10 23:26:20 -0500

    more water droplets one matrix
    -still work in progress, i need to add a continous water loop.

--- Simulations/Rainfall Simulations.py
+++ Simulations/Rainfall Simulations.py
@@ -1,5 +1,5 @@
 """
-Creator: Ojas Patel
+Creator: Ojas Patel, Carlos Fernandez
 Description: Script to simulate using an LED matrix as primary visual output
 """
 
@@ -8,16 +8,6 @@ from string import ascii_lowercase
 import pygame
 from pygame.locals import *
 import time
-
-pygame.init()
-clock = pygame.time.Clock()
-
-#create window and background
-screen_size = width, height = 640, 480
-screen = pygame.display.set_mode(screen_size)
-background = pygame.Surface((screen_size))
-LED_NUM = 8
-
 
 class ColorPalette:
     """
@@ -59,14 +49,16 @@ class LED:
     def __init__(self):
         self.led_grid = {}
         self.led_num = 8
+
     def create_led_grid(self):
         x_origin, y_origin = 145, 65
-        for letter, coord in zip(ascii_lowercase[0:8], range(LED_NUM)):
+        for letter, coord in zip(ascii_lowercase[0:self.led_num], range(self.led_num)):
             x = x_origin + (coord * 50)
             self.led_grid[letter + '1'] = [pygame.draw.circle(background, (ColorPalette().active_color), (x, y_origin), 20), False, x, y_origin]
-            for coord in range(1, LED_NUM):
+            for coord in range(1, self.led_num):
                 y = y_origin + (coord * 50)
                 self.led_grid[letter + str(coord+1)] = [pygame.draw.circle(background, (ColorPalette().active_color), (x, y), 20), False, x, y]
+        print(self.led_grid)
 
     def toggle_led(self):
         for led in self.led_grid.values():
@@ -79,7 +71,7 @@ class LED:
 
     def rain_drop(self):
         x_origin, y_origin = 145, 65
-        for letter, coord in zip(ascii_lowercase[0:8], range(LED_NUM)):
+        for letter, coord in zip(ascii_lowercase[0:self.led_num], range(self.led_num)):
             y = y_origin + (coord * 50)
             self.led_grid[letter + '1'] = [pygame.draw.circle(background, (0, 0, 255), (x_origin, y), 20), False, x_origin, y]
             pygame.display.update()
@@ -89,11 +81,64 @@ class LED:
             pygame.display.update()
             clock.tick(15)
 
+    def rain_simulation(self):
+        x_origin, y_origin = 145, 65
+        x2 = x_origin + (2*50) # two
+        x3 = x_origin + (4*50)
+        x4 = x_origin + (6*50)
+
+        x5 = x_origin + (1*50)
+        x6 = x_origin + (3*50)
+        x7 = x_origin + (5*50)
+        x8 = x_origin + (7*50)
+        for letter, coord in zip(ascii_lowercase[0:self.led_num], range(self.led_num)):
+            y = y_origin + (coord * 50)
+
+            #print(letter, coord)
+            self.led_grid[letter + '1'] = [pygame.draw.circle(background, (0, 0, 255), (x_origin, y), 20), False, x_origin, y]
+            self.led_grid[letter + '1'] = [pygame.draw.circle(background, (0, 0, 255), (x2, y), 20), False, x2, y]
+            self.led_grid[letter + '1'] = [pygame.draw.circle(background, (0, 0, 255), (x3, y), 20), False, x3, y]
+            self.led_grid[letter + '1'] = [pygame.draw.circle(background, (0, 0, 255), (x4, y), 20), False, x4, y]
+            if coord >= 1:
+                new_coord = coord-1
+                y_lag = y_origin +(new_coord *50)
+                self.led_grid[letter + '1'] = [pygame.draw.circle(background, (0, 0, 255), (x5, y_lag), 20), False, x5, y_lag]
+                self.led_grid[letter + '1'] = [pygame.draw.circle(background, (0, 0, 255), (x6, y_lag), 20), False, x6, y_lag]
+                self.led_grid[letter + '1'] = [pygame.draw.circle(background, (0, 0, 255), (x7, y_lag), 20), False, x7, y_lag]
+                self.led_grid[letter + '1'] = [pygame.draw.circle(background, (0, 0, 255), (x8, y_lag), 20), False, x8, y_lag]
+
+            pygame.display.update()
+            clock.tick(10)
+            screen.blit(background, (0, 0))
+            self.led_grid[letter + '1'] = [pygame.draw.circle(background, (255, 255, 255), (x_origin, y), 20), False, x_origin, y]
+            self.led_grid[letter + '1'] = [pygame.draw.circle(background, (255, 255, 255), (x2, y), 20), False, x2, y]
+            self.led_grid[letter + '1'] = [pygame.draw.circle(background, (255, 255, 255), (x3, y), 20), False, x3, y]
+            self.led_grid[letter + '1'] = [pygame.draw.circle(background, (255, 255, 255), (x4, y), 20), False, x4, y]
+
+            if coord >= 1:
+                self.led_grid[letter + '1'] = [pygame.draw.circle(background, (255, 255, 255), (x5, y_lag), 20), False, x5, y_lag]
+                self.led_grid[letter + '1'] = [pygame.draw.circle(background, (255, 255, 255), (x6, y_lag), 20), False, x6, y_lag]
+                self.led_grid[letter + '1'] = [pygame.draw.circle(background, (255, 255, 255), (x7, y_lag), 20), False, x7, y_lag]
+                self.led_grid[letter + '1'] = [pygame.draw.circle(background, (255, 255, 255), (x8, y_lag), 20), False, x8, y_lag]
+
+            pygame.display.update()
+            clock.tick(10)
+
+
+
+
 if __name__ == '__main__':
-    ColorPalette().create_palette()
-    LED().create_led_grid()
+    pygame.init()
+    clock = pygame.time.Clock()
+    #create window and background
+    screen_size = width, height = 640, 480
+    #screen_size = width, height = 1020, 960
+    screen = pygame.display.set_mode(screen_size)
+    background = pygame.Surface((screen_size))
+    LED = LED()
+    LED.create_led_grid()
     #core loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
-        LED().rain_drop()
+        LED.rain_simulation()
